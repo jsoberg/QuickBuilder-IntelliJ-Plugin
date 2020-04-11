3d67ea87693362babbc447d0b4f75ee083c2f356
Joshua Soberg <j15soberg@gmail.com>
2020-04-10 21:29:06 -0400

    Add a method generator to generate set methods for a Builder class

--- src/main/java/com/soberg/dev/quickbuilder/generation/BuilderGenerator.java
+++ src/main/java/com/soberg/dev/quickbuilder/generation/BuilderGenerator.java
@@ -7,18 +7,21 @@ public class BuilderGenerator {
 
     private final ClassGenerator classGenerator;
     private final FieldGenerator fieldGenerator;
+    private final MethodGenerator methodGenerator;
 
     public BuilderGenerator(Project project) {
         JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
         PsiElementFactory elementFactory = psiFacade.getElementFactory();
         this.classGenerator = new ClassGenerator(elementFactory);
         this.fieldGenerator = new FieldGenerator(elementFactory);
+        this.methodGenerator = new MethodGenerator(elementFactory);
     }
 
     public PsiClass generateBuilderClass(PsiClass sourceClass) throws BuilderGenerationException {
         PsiField[] sourceFields = sourceClass.getFields();
         PsiClass builderClass = classGenerator.generateBuilderClass();
         fieldGenerator.addBuilderFields(builderClass, sourceFields);
+        methodGenerator.addSetMethods(builderClass, sourceFields);
         return builderClass;
     }
 }
---
+++ src/main/java/com/soberg/dev/quickbuilder/generation/MethodGenerator.java
@@ -0,0 +1,45 @@
+package com.soberg.dev.quickbuilder.generation;
+
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiMethod;
+
+class MethodGenerator {
+
+    private final PsiElementFactory elementFactory;
+
+    MethodGenerator(PsiElementFactory elementFactory) {
+        this.elementFactory = elementFactory;
+    }
+
+    void addSetMethods(PsiClass builderClass, PsiField[] fields) {
+        for (PsiField field : fields) {
+            addSetMethodToBuilder(builderClass, field);
+        }
+    }
+
+    private void addSetMethodToBuilder(PsiClass builderClass, PsiField field) {
+        String fieldName = field.getName();
+        if (fieldName == null) {
+            throw new IllegalStateException("Could not find name for field " + field);
+        }
+        String methodName = generateSetMethodName(fieldName);
+        String fieldType = field.getType().getPresentableText();
+        String methodText = "public Builder " + methodName + "(" + fieldType + " " + fieldName + ")"
+                + " { this." + fieldName + " = " + fieldName + "; return this; }";
+        PsiMethod setMethod = elementFactory.createMethodFromText(methodText, field);
+        builderClass.add(setMethod);
+    }
+
+    private String generateSetMethodName(String fieldName) {
+        String methodAppend = toInitialUpperCase(fieldName);
+        return "set" + methodAppend;
+    }
+
+    private String toInitialUpperCase(String name) {
+        char initialChar = Character.toUpperCase(name.charAt(0));
+        // Check that our field name isn't a single character.
+        return initialChar + ((name.length() != 1) ? name.substring(1) : "");
+    }
+}
