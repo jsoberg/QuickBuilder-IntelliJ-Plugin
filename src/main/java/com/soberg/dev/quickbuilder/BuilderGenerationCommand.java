4335700f523460ed0b2cbc2ba1530f69274ae074
Joshua Soberg <j15soberg@gmail.com>
2020-04-14 19:39:11 -0400

    Begin using the constructor generator

--- src/main/java/com/soberg/dev/quickbuilder/BuilderGenerationCommand.java
+++ src/main/java/com/soberg/dev/quickbuilder/BuilderGenerationCommand.java
@@ -5,19 +5,23 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiMethod;
 import com.soberg.dev.quickbuilder.generation.BuilderGenerationException;
 import com.soberg.dev.quickbuilder.generation.BuilderGenerator;
+import com.soberg.dev.quickbuilder.generation.ConstructorGenerator;
 
 class BuilderGenerationCommand {
 
     private final Project project;
     private final BuilderGenerator builderGenerator;
+    private final ConstructorGenerator constructorGenerator;
     private final CommandProcessor processor;
     private final Application application;
 
     BuilderGenerationCommand(Project project) {
         this.project = project;
         this.builderGenerator = new BuilderGenerator(project);
+        this.constructorGenerator = new ConstructorGenerator(project);
         this.processor = CommandProcessor.getInstance();
         this.application = ApplicationManager.getApplication();
     }
@@ -39,7 +43,9 @@ class BuilderGenerationCommand {
     private void generateBuilder(PsiClass sourceClass) {
         try {
             PsiClass builderClass = builderGenerator.generateBuilderClass(sourceClass);
+            PsiMethod constructor = constructorGenerator.generatePrivateConstructor(sourceClass, builderClass);
             sourceClass.add(builderClass);
+            sourceClass.add(constructor);
         } catch (BuilderGenerationException e) {
             System.err.println("Problem generating builder for source class " + sourceClass.getName());
             e.printStackTrace();
