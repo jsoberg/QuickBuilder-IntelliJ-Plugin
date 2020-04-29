6d81ea70058a5e18efe86596b98fe803b537c8fa
Joshua Soberg <j15soberg@gmail.com>
2020-04-28 20:23:28 -0400

    #2 - Add initial tests for the BuilderGenerationCommand

---
+++ src/test/java/com/soberg/dev/quickbuilder/generation/BuilderGenerationCommandTest.java
@@ -0,0 +1,79 @@
+package com.soberg.dev.quickbuilder.generation;
+
+
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiMethod;
+import com.soberg.dev.quickbuilder.QuickBuilderNotifier;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.STRICT_STUBS)
+public class BuilderGenerationCommandTest {
+
+    @Mock
+    private Project project;
+    @Mock
+    private BuilderGenerator builderGenerator;
+    @Mock
+    private ConstructorGenerator constructorGenerator;
+    @Mock
+    private CommandProcessor processor;
+    @Mock
+    private Application application;
+    @Mock
+    private QuickBuilderNotifier notifier;
+
+    @Captor
+    private ArgumentCaptor<Runnable> runnableCaptor;
+
+    private BuilderGenerationCommand command;
+
+    @BeforeEach
+    public void setup() {
+        command = new BuilderGenerationCommand(project, builderGenerator, constructorGenerator, processor, application, notifier);
+    }
+
+    @Test
+    public void execute() throws BuilderGenerationException {
+        PsiClass sourceClass = mock(PsiClass.class);
+        command.execute(sourceClass);
+
+        // Execute should trigger Application.runWriteAction
+        verifyNoInteractions(application);
+        verifyNoInteractions(constructorGenerator);
+        verifyNoInteractions(builderGenerator);
+        verifyNoInteractions(sourceClass);
+        verify(processor).executeCommand(eq(project), runnableCaptor.capture(), eq("QuickBuilder-Generator"), eq(command));
+
+        // runWriteAction should trigger the actual builder generation
+        runnableCaptor.getValue().run();
+        verifyNoInteractions(constructorGenerator);
+        verifyNoInteractions(builderGenerator);
+        verifyNoInteractions(sourceClass);
+        verify(application).runWriteAction(runnableCaptor.capture());
+
+        PsiClass builderClass = mock(PsiClass.class);
+        when(builderGenerator.generateBuilderClass(sourceClass)).thenReturn(builderClass);
+        PsiMethod constructor = mock(PsiMethod.class);
+        when(constructorGenerator.generatePrivateConstructor(sourceClass, builderClass)).thenReturn(constructor);
+        runnableCaptor.getValue().run();
+        verify(builderGenerator).generateBuilderClass(sourceClass);
+        verify(constructorGenerator).generatePrivateConstructor(sourceClass, builderClass);
+        verify(sourceClass).add(builderClass);
+        verify(sourceClass).add(constructor);
+    }
+}
