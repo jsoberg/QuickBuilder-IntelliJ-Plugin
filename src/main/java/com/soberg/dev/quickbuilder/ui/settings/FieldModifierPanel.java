f5bae0a65592157e7b42e73afaccf481f71a7856
Joshua Soberg <j15soberg@gmail.com>
2020-05-07 21:23:19 -0400

    #1 - Cleanup the FieldModifierPanel, and actually modify preferences when the radio button changes

--- src/main/java/com/soberg/dev/quickbuilder/ui/settings/FieldModifierPanel.java
+++ src/main/java/com/soberg/dev/quickbuilder/ui/settings/FieldModifierPanel.java
@@ -1,33 +1,45 @@
 package com.soberg.dev.quickbuilder.ui.settings;
 
 import com.intellij.ui.IdeBorderFactory;
+import com.soberg.dev.quickbuilder.ui.settings.SettingsPreferences.FieldModifier;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
 
 class FieldModifierPanel {
 
-    FieldModifierPanel() {
+    private final SettingsPreferences preferences;
+
+    FieldModifierPanel(SettingsPreferences preferences) {
+        this.preferences = preferences;
     }
 
-    JPanel getPanel() {
+    JPanel createPanel() {
         JPanel panel = new JPanel();
         panel.setBorder(IdeBorderFactory.createTitledBorder("Builder Field Modifier"));
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 
-        ButtonGroup group = new ButtonGroup();
-        JRadioButton privateButton = new JRadioButton("private");
-        group.add(privateButton);
-        panel.add(privateButton);
-        JRadioButton packagePrivateButton = new JRadioButton("package-private");
-        group.add(packagePrivateButton);
-        panel.add(packagePrivateButton);
-        JRadioButton publicButton = new JRadioButton("public");
-        group.add(publicButton);
-        panel.add(publicButton);
+        SettingsPreferences.State state = preferences.getState();
+        addRadioButtons(panel, state);
 
         panel.add(Box.createHorizontalGlue());
         panel.setMinimumSize(new Dimension(Short.MAX_VALUE, 0));
         return panel;
+    }
+
+    private void addRadioButtons(JPanel panel, SettingsPreferences.State state) {
+        ButtonGroup group = new ButtonGroup();
+        for (FieldModifier modifier : FieldModifier.values()) {
+            boolean selected = modifier == state.fieldModifier;
+            JRadioButton button = new JRadioButton(modifier.displayName, selected);
+            button.addItemListener(e -> {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    preferences.setFieldModifier(modifier);
+                }
+            });
+            panel.add(button);
+            group.add(button);
+        }
     }
 }
