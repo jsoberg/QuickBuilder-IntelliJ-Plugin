88617842fb9df5697f8be79181015801c0e2d7dc
Joshua Soberg <j15soberg@gmail.com>
2020-05-07 21:23:45 -0400

    #1 - Cleanup the QuickBuilderSettingsPanel

--- src/main/java/com/soberg/dev/quickbuilder/ui/settings/QuickBuilderSettingsPanel.java
+++ src/main/java/com/soberg/dev/quickbuilder/ui/settings/QuickBuilderSettingsPanel.java
@@ -1,14 +1,24 @@
 package com.soberg.dev.quickbuilder.ui.settings;
 
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.options.Configurable;
-import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class QuickBuilderSettingsPanel implements Configurable {
+
+    private final SettingsPreferences preferences;
+    private final FieldModifierPanel fieldModifierPanel;
+
+    public QuickBuilderSettingsPanel(Project project) {
+        this.preferences = ServiceManager.getService(project, SettingsPreferences.class);
+        this.fieldModifierPanel = new FieldModifierPanel(preferences);
+    }
 
     @Nls(capitalization = Nls.Capitalization.Title)
     @Override
@@ -19,23 +29,39 @@ public class QuickBuilderSettingsPanel implements Configurable {
     @Nullable
     @Override
     public JComponent createComponent() {
+        JPanel panel = createMainPanel();
+        GridBagConstraints constraints = createGridBagConstraints();
+        // Builder field modifiers
+        panel.add(fieldModifierPanel.createPanel(), constraints);
+
+        constraints.weighty = 1;
+        constraints.fill = GridBagConstraints.BOTH;
+        addFillerPanel(panel, constraints);
+        return panel;
+    }
+
+    @NotNull
+    private JPanel createMainPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(new GridBagLayout());
+        return panel;
+    }
+
+    @NotNull
+    private GridBagConstraints createGridBagConstraints() {
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.NORTH;
         constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.weightx = 1;
         constraints.gridx = 0;
         constraints.gridy = 0;
+        return constraints;
+    }
 
-        panel.add(new FieldModifierPanel().getPanel(), constraints);
-
-        constraints.weighty = 1;
-        constraints.fill = GridBagConstraints.BOTH;
+    private void addFillerPanel(JPanel parent, GridBagConstraints constraints) {
         JPanel filler = new JPanel();
         filler.setOpaque(false);
-        panel.add(filler, constraints);
-        return panel;
+        parent.add(filler, constraints);
     }
 
     @Override
@@ -44,7 +70,6 @@ public class QuickBuilderSettingsPanel implements Configurable {
     }
 
     @Override
-    public void apply() throws ConfigurationException {
-
+    public void apply() {
     }
 }
